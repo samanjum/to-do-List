46723b2cce5f7cea12931e56f08236261ea95595
Sameer Anjum <109406939+samanjum@users.noreply.github.com>
2024-09-07 14:53:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const inputBox = document.querySelector("#inputBox"); // Correct ID
+const listContainer = document.querySelector("#list-container");
+
+function addTask() {
+  if (inputBox.value === '') {
+    alert('You Must Write Something');
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = "\u00d7"
+li.appendChild(span)
+    // Clear the input field after adding the task
+    // inputBox.value = '';
+  }
+  inputBox.value = '';
+  saveData();
+}
+listContainer.addEventListener("click",function(e){
+  if(e.target.tagName.toLowerCase() === "li"){
+    e.target.classList.toggle("checked")
+    saveData();
+  }
+else if(e.target.tagName.toLowerCase() === "span"){
+  e.target.parentElement.remove();
+  saveData();
+
+}
+},false)
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showData();
